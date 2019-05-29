ef1e27fe73dff0be72a58967387a5782dae5465a
irvs_t_hayashi <t_hayashi@irvs.ait.kyushu-u.ac.jp>
2019-05-29 14:08:47 +0900

    AR Core SDK for Unity v1.9.0導入 MainScript追加 基本の部品配置完了

---
+++ Assets/Scripts/MainScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour {
+
+	public bool ScreenNOTSleep;
+
+	// Use this for initialization
+	void Start () {
+		// 画面が消えないようにする
+		if (ScreenNOTSleep) {
+			Screen.sleepTimeout = SleepTimeout.NeverSleep;
+		}
+		else {
+			Screen.sleepTimeout = SleepTimeout.SystemSetting;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// 戻るボタンでアプリ終了
+		if (Input.GetKey(KeyCode.Escape)) {
+			Application.Quit();
+		}
+	}
+}
