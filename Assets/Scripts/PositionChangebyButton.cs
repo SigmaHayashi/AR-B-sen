987bfe7ccf1d7e3550903acd51c63734d63746d8
irvs_t_hayashi <t_hayashi@irvs.ait.kyushu-u.ac.jp>
2019-05-30 15:06:24 +0900

    Augmented Imageを使って移動する機能を付けた（うまくいってない）

--- Assets/Scripts/PositionChangebyButton.cs
+++ Assets/Scripts/PositionChangebyButton.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using GoogleARCore;
 
 public class PositionChangebyButton : MonoBehaviour {
 
-	public GameObject whichObject;
+	private GameObject whichObject;
+	private GameObject childObject;
 
 	public Button posXplusButton;
 	public Button posXminusButton;
@@ -21,8 +23,15 @@ public class PositionChangebyButton : MonoBehaviour {
 	public Button rotZplusButton;
 	public Button rotZminusButton;
 
+	public Button autoPositioningButton;
+
 	public Text cameraPositionText;
 	public Text bsenPositionText;
+	public Text debugText;
+
+	private List<AugmentedImage> m_AugmentedImages = new List<AugmentedImage>();
+
+	private Dictionary<int, GameObject> m_dictionary = new Dictionary<int, GameObject>();
 
 	// Use this for initialization
 	void Start () {
@@ -39,6 +48,11 @@ public class PositionChangebyButton : MonoBehaviour {
 		rotYminusButton.onClick.AddListener(onRotYminusClick);
 		rotZplusButton.onClick.AddListener(onRotZplusClick);
 		rotZminusButton.onClick.AddListener(onRotZminusClick);
+
+		autoPositioningButton.onClick.AddListener(onAutoPositioningClick);
+
+		whichObject = GameObject.Find("rostms");
+		childObject = GameObject.Find("rostms/world_link");
 	}
 	
 	// Update is called once per frame
@@ -48,42 +62,120 @@ public class PositionChangebyButton : MonoBehaviour {
 
 		bsenPositionText.text = "B-sen Position : " + whichObject.transform.position.ToString() + "\n";
 		bsenPositionText.text += "B-sen Rotation : " + whichObject.transform.eulerAngles.ToString();
+
+
+		Session.GetTrackables<AugmentedImage>(m_AugmentedImages, TrackableQueryFilter.Updated);
+
+		GameObject tmp_object = null;
+		var image = m_AugmentedImages[0];
+		m_dictionary.TryGetValue(image.DatabaseIndex, out tmp_object);
+
+		if(image.TrackingState == TrackingState.Tracking && tmp_object == null) {
+			m_dictionary.Add(image.DatabaseIndex, whichObject);
+
+			debugText.text = "Auto Positioning READY";
+		}
+	}
+
+	void autoPositioning() {
+		GameObject tmp_object = null;
+		var image = m_AugmentedImages[0];
+		m_dictionary.TryGetValue(image.DatabaseIndex, out tmp_object);
+		if(image.TrackingState == TrackingState.Tracking && tmp_object != null) {
+			//GameObject tmp_object2 = new GameObject();
+			//tmp_object2.transform.position = image.CenterPose.position;
+			//tmp_object2.transform.eulerAngles = image.CenterPose.rotation.eulerAngles;
+
+			Vector3 tmp_euler = image.CenterPose.rotation.eulerAngles;
+			//Vector3 tmp_euler = tmp_object2.transform.eulerAngles;
+			tmp_euler.x = 0.0f;
+			tmp_euler.y += 90.0f;
+			tmp_euler.z = 0.0f;
+			//tmp_object2.transform.eulerAngles = tmp_euler;
+
+			Vector3 temp_position = image.CenterPose.position;
+			//Vector3 temp_position = tmp_object2.transform.position;
+			Vector3 temp_position_sub = new Vector3(-6.2f, 1.6f, 9.2f);
+			temp_position_sub -= temp_position;
+
+			whichObject.transform.eulerAngles = tmp_euler;
+			childObject.transform.localPosition = temp_position_sub;
+		}
+	}
+
+	void onAutoPositioningClick() {
+		autoPositioning();
 	}
 
 	void onPosXplusClick() {
+		/*
 		Vector3 tmp = whichObject.transform.position;
 		tmp.x += 0.05f;
 		whichObject.transform.position = tmp;
+		*/
+
+		Vector3 tmp = childObject.transform.localPosition;
+		tmp.x += 0.05f;
+		childObject.transform.localPosition = tmp;
 	}
 
 	void onPosXminusClick() {
+		/*
 		Vector3 tmp = whichObject.transform.position;
 		tmp.x -= 0.05f;
 		whichObject.transform.position = tmp;
+		*/
+
+		Vector3 tmp = childObject.transform.localPosition;
+		tmp.x -= 0.05f;
+		childObject.transform.localPosition = tmp;
 	}
 
 	void onPosYplusClick() {
+		/*
 		Vector3 tmp = whichObject.transform.position;
 		tmp.y += 0.05f;
 		whichObject.transform.position = tmp;
+		*/
+
+		Vector3 tmp = childObject.transform.localPosition;
+		tmp.y += 0.05f;
+		childObject.transform.localPosition = tmp;
 	}
 
 	void onPosYminusClick() {
+		/*
 		Vector3 tmp = whichObject.transform.position;
 		tmp.y -= 0.05f;
 		whichObject.transform.position = tmp;
+		*/
+
+		Vector3 tmp = childObject.transform.localPosition;
+		tmp.y -= 0.05f;
+		childObject.transform.localPosition = tmp;
 	}
 
 	void onPosZplusClick() {
+		/*
 		Vector3 tmp = whichObject.transform.position;
 		tmp.z += 0.05f;
 		whichObject.transform.position = tmp;
-	}
+		*/
 
+		Vector3 tmp = childObject.transform.localPosition;
+		tmp.z += 0.05f;
+		childObject.transform.localPosition = tmp;
+	}
 	void onPosZminusClick() {
+		/*
 		Vector3 tmp = whichObject.transform.position;
 		tmp.z -= 0.05f;
 		whichObject.transform.position = tmp;
+		*/
+
+		Vector3 tmp = childObject.transform.localPosition;
+		tmp.z -= 0.05f;
+		childObject.transform.localPosition = tmp;
 	}
 
 
