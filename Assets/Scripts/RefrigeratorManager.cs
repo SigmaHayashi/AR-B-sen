dbc1d26672f74484c4fb1d208a5cb47a2f426443
irvs_t_hayashi <t_hayashi@irvs.ait.kyushu-u.ac.jp>
2019-06-17 21:58:22 +0900

    Coordinates Adapterを利用しての距離計測に変更することで，もののゼロ点どうしではなくオフセットをつけての計測が可能に X,Z座標だけをもとに計算する方式に変更（Y座標の距離は無視）

--- Assets/Scripts/RefrigeratorManager.cs
+++ Assets/Scripts/RefrigeratorManager.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RefrigeratorManager : MonoBehaviour {
 
 	private GameObject refrigerator;
 	private GameObject ar_camera;
+	
+	private GameObject coordinates_adapter;
 
 	private GameObject greentea;
 	private GameObject cancoffee;
 	private GameObject soysauce;
 
 	private float distance;
+
+	//public Text debug_text;
 
 	// Start is called before the first frame update
 	void Start() {
@@ -25,11 +30,18 @@ public class RefrigeratorManager : MonoBehaviour {
 		greentea.SetActive(false);
 		cancoffee.SetActive(false);
 		soysauce.SetActive(false);
+
+		GameObject prefab = (GameObject)Resources.Load("Coordinates Adapter");
+		coordinates_adapter = (GameObject)Instantiate(prefab, this.transform);
+		coordinates_adapter.transform.parent = refrigerator.transform;
 	}
 
 	// Update is called once per frame
 	void Update() {
-		distance = Vector3.Distance(refrigerator.transform.position, ar_camera.transform.position);
+		//distance = Vector3.Distance(refrigerator.transform.position, ar_camera.transform.position);
+		//distance = Mathf.Sqrt(Mathf.Pow((refrigerator_pos.x - ar_camera.transform.position.x), 2) + Mathf.Pow((refrigerator_pos.z - ar_camera.transform.position.z), 2));
+		coordinates_adapter.transform.localPosition = new Vector3(-0.23f, 0.0f, -0.3f);
+		distance = CalcDistance(coordinates_adapter, ar_camera);
 
 		if(distance < 1.5f) {
 			greentea.SetActive(true);
@@ -41,5 +53,13 @@ public class RefrigeratorManager : MonoBehaviour {
 			cancoffee.SetActive(false);
 			soysauce.SetActive(false);
 		}
+
+		//debug_text.text = distance.ToString("f2");
+	}
+
+	float CalcDistance(GameObject obj_a, GameObject obj_b) {
+		Vector3 obj_a_pos = obj_a.transform.position;
+		Vector3 obj_b_pos = obj_b.transform.position;
+		return Mathf.Sqrt(Mathf.Pow((obj_a_pos.x - obj_b_pos.x), 2) + Mathf.Pow((obj_a_pos.z - obj_b_pos.z), 2));
 	}
 }
