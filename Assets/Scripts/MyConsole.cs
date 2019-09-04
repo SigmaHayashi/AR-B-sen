881e23050cdd64f701a8207c6754d9643fc53887
irvs_t_hayashi <t_hayashi@irvs.ait.kyushu-u.ac.jp>
2019-09-04 21:18:25 +0900

    Canvasを追加 今後UIを変更していく予定

---
+++ Assets/Scripts/MyConsole.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyConsole : MonoBehaviour {
+	
+	public GameObject TextObject;
+
+	private RectTransform text_rect;
+	private Text text_text;
+
+	// Start is called before the first frame update
+	void Start() {
+		text_rect = TextObject.GetComponent<RectTransform>();
+		text_text = TextObject.GetComponent<Text>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		Vector2 delta_size = text_rect.sizeDelta;
+		if (delta_size.y > 0) {
+			Vector2 new_pivot = new Vector2(0, 0);
+			text_rect.pivot = new_pivot;
+		}
+		else {
+			Vector2 new_pivot = new Vector2(0, 1);
+			text_rect.pivot = new_pivot;
+		}
+
+		if(delta_size.y > 10000) {
+			int index = text_text.text.IndexOf("\n", 1);
+			text_text.text = text_text.text.Remove(1, index);
+		}
+	}
+
+	public void Add(object message) {
+		text_text.text += message.ToString() + "\n";
+	}
+
+	public void Delete() {
+		text_text.text = "\n";
+	}
+}
